b75d57a2c5af3ae48701f0c9d4e8fd650aee2a6a
Kamila Silva <kamiladaf@yahoo.com.br>
2025-04-26 02:17:38 -0300

    Corrige importação do botão para letra maiúscula

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': '/src', // ← ✅ caminho direto, sem "path"
     },
   },
   server: {
     host: true,
     port: 5173,
     strictPort: false,
-    allowedHosts: 'all' // ✅ CORRETO aqui
+    allowedHosts: 'all'
   }
 })
