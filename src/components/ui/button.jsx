009da3ee316f0b515237d80739dd8e36215f0a5d
Kamila Silva <kamiladaf@yahoo.com.br>
2025-04-26 16:10:53 -0300

    Adicionando button.jsx novo

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -1,7 +1,9 @@
+import React from "react";
+
 export function Button({ children, className = "", ...props }) {
   return (
     <button
-      className={`px-4 py-2 rounded-md font-semibold focus:outline-none transition duration-200 ${className}`}
+      className={`px-4 py-2 rounded-lg shadow bg-green-600 hover:bg-green-700 text-white font-semibold ${className}`}
       {...props}
     >
       {children}
