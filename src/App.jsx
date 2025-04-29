3546e306911dab8b150da41a691fa2c2efe403a4
Kamila Silva <kamiladaf@yahoo.com.br>
2025-04-29 18:23:12 -0300

    Recriando App.jsx limpo e único

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Apresentacao from "./components/ui/Apresentacao";
+import PainelTransparencia from "./components/PainelTransparencia";
+import Certificado from "./Certificado";
+import Configuracoes from "./Configuracoes";
+import Relatorios from "./Relatorios";
+import Layout from "./layout/Layout";
+import ContatoFlutuante from "@/components/ui/ContatoFlutuante";
+import Header from "@/components/ui/Header";
+import Vitrine from "./Vitrine";
+import ESGAnalyzer from "./components/ESGAnalyzer";
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div className="pt-24">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Vitrine />} />
+            <Route path="analisar" element={<ESGAnalyzer />} />
+            <Route path="apresentacao" element={<Apresentacao />} />
+            <Route path="painel" element={<PainelTransparencia />} />
+            <Route path="certificado" element={<Certificado />} />
+            <Route path="configuracoes" element={<Configuracoes />} />
+            <Route path="relatorios" element={<Relatorios />} />
+          </Route>
+        </Routes>
+      </div>
+      <ContatoFlutuante />
+    </>
+  );
+}
